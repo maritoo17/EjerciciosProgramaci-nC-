22bd1a6eeade26ab04abf8a3d227ec89760d71b2
sergiioo101 <smartros@myuax.com>
2023-10-23 19:59:48 +0200

    tiposdatos

---
+++ ejercicio 1/tiposdatos.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    int numeroentero = 33;
+    std::cout << "Numero entero: " << numeroentero << std::endl;
+
+    double numerodecimal = 3.1415;
+    std::cout << "Numero decimal: " << numerodecimal << std::endl;
+
+    char letter = 'S';
+    std::cout << "Caracter: " << letter << std::endl;
+
+    bool verdadero = true;
+    bool falso = false;
+
+    std::cout << "Aprobare el examen de programacion:" << verdadero << std::endl;
+    std::cout << "Fernando Alonso ganara la 33:" << falso << std::endl;
+
+    return  0;
+
+}
